ca92e3ede0768ce9c348e59c628410966f08bcfa
Matheus Gomes <141504911+MatheusGomes444@users.noreply.github.com>
2024-11-22 21:18:30 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,7 +4,31 @@ const registrolink = document.querySelector('.registro-link');
 const btnLoginpopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 const items = document.querySelectorAll('.carousel-item');
+let numberOfSlides = 4;
+const right = document.querySelector('.right');
+const left = document.querySelector('.left');
 
+const slider = document.querySelector('.slider');
+
+let sectionIndex = 0;
+function reset() {
+    for (let i=0; i<slider.children.length; i++) {
+        slider.children[i].style.opacity = 0;
+        slider.children[i].style.zIndex = 0;
+    }
+}
+right.addEventListener('click', function () {
+    reset();
+    sectionIndex = (sectionIndex < numberOfSlides-1) ? sectionIndex + 1 : numberOfSlides-1;
+    slider.children[sectionIndex].style.zIndex = 1;
+    slider.children[sectionIndex].style.opacity = 1;
+});
+left.addEventListener('click', function () {
+    reset();
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    slider.children[sectionIndex].style.zIndex = 1;
+    slider.children[sectionIndex].style.opacity = 1;
+});
 function toggleModal() {
     const wrapper = document.querySelector('.wrapper');
     wrapper.classList.toggle('active');
